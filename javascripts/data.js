25d20550c234c294329d769d9ecf7311dece3c12
anhv <brother.rain.1024@gmail.com>
2015-06-30 09:47:34 +0700

    add bower

--- javascripts/data.js
+++ javascripts/data.js
@@ -21,6 +21,31 @@ svg.append("rect")
 d3.json("data/data.json", function(error, graph) {
     if (error) throw error;
 
+
+    var groups;
+    //noinspection JSUnusedAssignment
+    groups = svg.selectAll(".group").data(graph.groups)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return d.x })
+        .attr("y", function (d) { return d.y })
+        .attr("width", 160)
+        .attr("height", 25)
+        .attr("fill", "#7F2A68")
+        .style("stroke", "white");
+
+    var labelGroups;
+    labelGroups = svg.selectAll(".labelGroups").data(graph.groups)
+        .enter().append("text")
+        .attr("fill", "white")
+        .attr("align", "center")
+        .attr("x", function (d) { return d.x + 7 })
+        .attr("y", function (d) { return d.y + 18 })
+        .style("cursor", "hand")
+        .text(function (d) {
+            return d.name;
+        });
+
     var link = svg.selectAll(".link")
         .data(graph.links)
         .enter()
@@ -29,50 +54,50 @@ d3.json("data/data.json", function(error, graph) {
             .style("stroke", "#7F2A68")
             .style("stroke-width", 6);
 
-    var node = svg.selectAll(".node")
-        .data(graph.nodes)
-        .enter()
-        .append("circle")
+    var node = svg.selectAll(".node").data(graph.nodes)
+        .enter().append("circle")
             .attr("class", "node")
             .attr("r", 6)
             .style("stroke", "#9E698F")
             .style("stroke-width", 3)
-            .style("fill", "white")
-        .call(force.drag);
+            .style("fill", "white");
 
-    node.on("mouseover", function(d){
+    node.on("mouseover", function(){
         d3.select(this)
-            .attr("r", 12);
+            .attr("r", 9);
     });
 
-    node.on("mouseleave", function(d){
+    node.on("mouseleave", function(){
         d3.select(this)
             .attr("r", 6);
-    })
+    });
 
     node.append('title')
         .text(function(d) { return d.name; });
 
     var labels = svg.selectAll("text.lable")
-        .data(graph.nodes)
-        .enter().append("text")
+        .data(graph.nodes).enter().append("text")
         .attr("class", "label")
         .attr("fill", "white")
         .attr("color", "white")
         .attr("align", "center")
+        .style("cursor", "hand")
         .text(function(d) {  return d.name;  });
-
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
 
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
 
+    function getNode(index){
+        return d3.selectAll("circle")[0][index].attributes;
+    }
+
+    link.attr("x1", function(d) { return getNode(d.source).cx.value; })
+        .attr("y1", function(d) { return getNode(d.source).cy.value ; })
+        .attr("x2", function(d) { return getNode(d.target).cx.value; })
+        .attr("y2", function(d) { return getNode(d.target).cy.value ; });
+
+
     labels.attr("transform", function(d){
         return "translate(" + (d.x + 10) + "," + (d.y + 5) + ")";
-    })
-    //force.on("tick", function() {
-    //});
+    });
 });
